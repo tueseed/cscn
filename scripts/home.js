082dab3f1eb45c0d7f5dfaaa2d7bcbe2f37f4136
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-03-29 19:48:33 +0700

    fdadaf

--- scripts/home.js
+++ scripts/home.js
@@ -7,6 +7,7 @@ if(!getUrlVars()["code"])
 else if(getUrlVars()["code"])
 {
   var code = getUrlVars()["code"]
+  console.log(code)
   $.ajax({
     async: true,
     crossDomain: true,
@@ -26,7 +27,7 @@ else if(getUrlVars()["code"])
     statusCode:{
                 400:function()
                     {
-                      alert("400");
+                      console.log('400')
                       window.location.href = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654004533&redirect_uri=https%3A%2F%2Fcscn.herokuapp.com%3Faction%3Dhome&state=12345abcd&scope=openid'
                     }
                },
