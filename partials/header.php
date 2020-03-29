653107ee2603dc6db1d635be1e437c015d3978ff
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-03-29 20:27:55 +0700

    fdateqaf

--- partials/header.php
+++ partials/header.php
@@ -35,7 +35,7 @@
             <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
               <span class="header text-dark font-weight-bold text-center" id="user_logged">
                 <span class="text-white" id="userName">ยังไม่ได้เข้าสู่ระบบ</span>
-                <img src="" id="userPicture" width="75" height="50">
+                <img class="shadow-sm" src="" id="userPicture" style="width:50px;height:50px;border-radius:50px 50px;"/>
               </span>
             </a>
             <div class="dropdown-menu dropdown-menu-right justify-content-center">
