b2869d8ec7f6b3c5e940cfe8ed034ec2f9467b80
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-03-29 18:58:59 +0700

    fkdl;skodpcv

--- scripts/global.js
+++ scripts/global.js
@@ -10,12 +10,7 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-if(!getUrlVars()["code"])
-{window.location.href= 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654004533&redirect_uri=https://cscn.herokuapp.com?action=home&state=12345abcd&scope=openid'}
-else if(getUrlVars()["code"])
-{
-  alert(getUrlVars()["code"])
-}
+
 
 
 
