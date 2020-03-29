2bd1c7f87dc667e9a87c3f680d682f3f0e78becc
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-03-29 19:50:09 +0700

    dsdsafds

--- scripts/home.js
+++ scripts/home.js
@@ -28,7 +28,7 @@ else if(getUrlVars()["code"])
                 400:function()
                     {
                       console.log('400')
-                      window.location.href = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654004533&redirect_uri=https%3A%2F%2Fcscn.herokuapp.com%3Faction%3Dhome&state=12345abcd&scope=openid'
+                      // window.location.href = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654004533&redirect_uri=https%3A%2F%2Fcscn.herokuapp.com%3Faction%3Dhome&state=12345abcd&scope=openid'
                     }
                },
     success: function(response) {
