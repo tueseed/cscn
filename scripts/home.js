bf17488ceb2db959c7078a235b5956e9da96705d
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-04-29 15:28:31 +0700

    mksdacjpadsc

--- scripts/home.js
+++ scripts/home.js
@@ -225,7 +225,8 @@ async function creat_job()
   var checkJob = await job.orderByChild('reqNumber').equalTo($('#reqnumberAdd').val()).once('value')
   var checkDocnumber = await number.endAt().limitToLast(1).once('value')
   var snapDocnumber = checkDocnumber.val()
-  if(snapDocnumber == null){var docno = 1}else{var docno = snapDocnumber[0].docnumber + 1}
+  var docno = 0
+  if(snapDocnumber == null){ docno = 1}else{ docno = snapDocnumber[0].docnumber + 1}
   if(checkJob.val()== null)
   {
       var pushJob = await job.push({
