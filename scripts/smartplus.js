6d36ebb0be00ee6adf4d8150ee36397c1413758b
tueseed <tue_seed@hotmail.com>
2021-01-21 11:14:51 +0700

    mkodjvadf

--- scripts/smartplus.js
+++ scripts/smartplus.js
@@ -2,8 +2,9 @@ $('#header').hide()
 liff
   .init({
     liffId: "1655595874-V1dyPa3Y" // Use own liffId
-  })
-liff.getOs().then(os => {
-    const getos = os
-    alert(getos + "vdsvj")
-})
+  }).then(liff.getOs())
+  
+// liff.getOs().then(os => {
+//     const getos = os
+//     alert(getos + "vdsvj")
+// })
