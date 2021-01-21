d4d1a7b3b380a20986c629d793d3621478926d5a
tueseed <tue_seed@hotmail.com>
2021-01-21 11:34:45 +0700

    vhjcihvu0adv

--- scripts/smartplus.js
+++ scripts/smartplus.js
@@ -5,8 +5,8 @@ liff
     liffId: "1655595874-0poKDg98" // Use own liffId
   })
   .then(() => {
-    liff.getProfile().then(profile => {
-                                        const name = profile.displayName
-                                        alert(name)
+    liff.getOs().then(osname => {
+                                        const os = osname
+                                        alert(os)
                             })
   })
