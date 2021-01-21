399bfca39d866c65425f291586691de2f1c1832e
tueseed <tue_seed@hotmail.com>
2021-01-21 11:38:43 +0700

    vncjkopavdfv

--- scripts/smartplus.js
+++ scripts/smartplus.js
@@ -4,9 +4,4 @@ liff
   .init({
     liffId: "1655595874-0poKDg98" // Use own liffId
   })
-  .then(() => {
-    liff.getOs().then(osname => {
-                                        const os = osname
-                                        alert(os)
-                            })
-  })
+  .then(alert(liff.getOs()))
