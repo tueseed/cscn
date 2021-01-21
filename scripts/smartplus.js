b8227dff0b65ba332ded9a37e3672e5eb519f0cc
tueseed <tue_seed@hotmail.com>
2021-01-21 11:26:20 +0700

    jio-jvadv

--- scripts/smartplus.js
+++ scripts/smartplus.js
@@ -2,7 +2,7 @@ $('#header').hide()
 liff
   .init({
     liffId: "1655595874-V1dyPa3Y" // Use own liffId
-  }).then(alert('vcmonjvp'))
+  }).then(liff.getOs())
 
 // liff.getOs().then(os => {
 //     const getos = os
