26353143fb6204da13c775a6c202793cf867dd08
tueseed <tue_seed@hotmail.com>
2021-01-21 23:20:09 +0700

    lkkopivdv

--- scripts/poweroutage.js
+++ scripts/poweroutage.js
@@ -45,7 +45,7 @@ function showPosition(position)
 function makemessage(lat,long,peano,place,observ,customername,tel)
 {
     var link = "https://liff.line.me/1655595874-1mYGWaze?lat=" + lat + "&long=" + long + "&peano=" + peano + "&place=" + place + "&observ=" + observ + "&customername=" + customername + "&tel=" + tel
-    alert(link.length())
+    alert(link.length)
     var mess = {
                 "type": "bubble",
                     "hero": {
