fcae6905d5843a9e679f67d7b7fa5d7cd676bdf7
tueseed <tue_seed@hotmail.com>
2021-01-21 17:41:03 +0700

    jviodhjpvadfv

--- smartplus.php
+++ smartplus.php
@@ -1,17 +1,25 @@
-<div class="row mt-3">
+    <div class="row mt-3">  
         <div class="col-lg-12">
             <div class="img-square-wrapper text-center" >
-                <img class="shadow" src="pic/ios.png" width="200" height="300" alt="Card image cap">
+                <img class="shadow" src="pic/ios.png" width="200" height="50" alt="Card image cap">
             </div>
         </div>
     </div>
     <div class="row mt-2">
         <div class="col-lg-12">
-            <span class="card-text">ชื่อหนังสือ</span>
+            <input class="btn btn-primary">Download</input>
+        </div>
+    </div>
+
+    <div class="row mt-3">  
+        <div class="col-lg-12">
+            <div class="img-square-wrapper text-center" >
+                <img class="shadow" src="pic/ios.png" width="200" height="50" alt="Card image cap">
+            </div>
         </div>
     </div>
     <div class="row mt-2">
         <div class="col-lg-12">
-            <span class="card-text">ผู้เขียน</span>
+        <input class="btn btn-primary">Download</input>
         </div>
     </div>
