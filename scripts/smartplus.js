a6bb7dccba1a2ef1b14cbf990d17194d9a191e71
tueseed <tue_seed@hotmail.com>
2021-01-21 17:37:36 +0700

    jvifohdvusdg

--- scripts/smartplus.js
+++ scripts/smartplus.js
@@ -4,4 +4,3 @@ liff
   .init({
     liffId: "1655595874-0poKDg98" // Use own liffId
   })
-  .then(alert(liff.getOs()))
---
+++ smartplus.php
@@ -0,0 +1,17 @@
+<div class="row mt-3">
+        <div class="col-lg-12">
+            <div class="img-square-wrapper text-center" >
+                <img class="shadow" src="pic/ios.png" width="200" height="300" alt="Card image cap">
+            </div>
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-lg-12">
+            <span class="card-text">ชื่อหนังสือ</span>
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-lg-12">
+            <span class="card-text">ผู้เขียน</span>
+        </div>
+    </div>
