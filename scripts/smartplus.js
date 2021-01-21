25a18ab8bad21c9bd107c8d2e5bcc33a27d0e6fd
tueseed <tue_seed@hotmail.com>
2021-01-21 11:24:34 +0700

    vnjfoinvadf

--- scripts/smartplus.js
+++ scripts/smartplus.js
@@ -2,8 +2,8 @@ $('#header').hide()
 liff
   .init({
     liffId: "1655595874-V1dyPa3Y" // Use own liffId
-  }).then(liff.getOs())
-  
+  }).then(alert('vcmonjvp'))
+
 // liff.getOs().then(os => {
 //     const getos = os
 //     alert(getos + "vdsvj")
