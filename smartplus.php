178fa6dfb540e7874526e4500850ae467afeca37
tueseed <tue_seed@hotmail.com>
2021-01-21 17:46:24 +0700

    jvfiojvsdfbv

--- smartplus.php
+++ smartplus.php
@@ -6,20 +6,20 @@
         </div>
     </div>
     <div class="row mt-2">
-        <div class="col-lg-12">
-            <input class="btn btn-primary">Download</input>
+        <div class="col-lg-12 text-center">
+            <button href="https://apps.apple.com/th/app/pea-smart-plus/id1063710354?l=th" class="btn btn-primary">Download</button>
         </div>
     </div>
 
     <div class="row mt-3">  
         <div class="col-lg-12">
             <div class="img-square-wrapper text-center" >
-                <img class="shadow" src="pic/ios.png" width="200" height="50" alt="Card image cap">
+                <img class="shadow" src="pic/android.png" width="200" height="50" alt="Card image cap">
             </div>
         </div>
     </div>
     <div class="row mt-2">
-        <div class="col-lg-12">
-        <input class="btn btn-primary">Download</input>
+        <div class="col-lg-12 text-center">
+        <button href="https://play.google.com/store/apps/details?id=com.esrith.pea_mobile&hl=th&gl=US" class="btn btn-primary">Download</button>
         </div>
     </div>
