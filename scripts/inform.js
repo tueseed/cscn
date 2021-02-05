3a734ddfae78f44dc290d94e4fafc67126bbbd7d
tueseed <tue_seed@hotmail.com>
2021-02-05 17:07:14 +0700

    bmkfombiosfgb

--- scripts/inform.js
+++ scripts/inform.js
@@ -7,7 +7,7 @@ function send_reqnumber()
     var formData = new FormData()
     formData.append('NO','JPTM64000283')
     $.ajax({
-                url: '',
+                url: 'https://eservice.pea.co.th/cos/checkstatus',
                 headers: {  'Access-Control-Allow-Origin': 'https://eservice.pea.co.th/cos/checkstatus' },
                 method: 'POST',
                 data:formData,
