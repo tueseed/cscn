8abff798de727782e6475db04863fa44d86af5d3
tueseed <tue_seed@hotmail.com>
2021-02-05 16:46:37 +0700

    bgkoj[bsif

--- scripts/inform.js
+++ scripts/inform.js
@@ -4,19 +4,20 @@ $('#header').hide()
 
 function send_reqnumber()
 {
-    var form = new FormData();
-    form.append("NO", "JPTM64000283");
-
-    var settings = {
-                    "url": "https://eservice.pea.co.th/cos/checkstatus/",
-                    "method": "POST",
-                    "timeout": 0,
-                    "processData": false,
-                    "mimeType": "multipart/form-data",
-                    "contentType": false,
-                    "data": form
-                    }
-    $.ajax(settings).done(function (response) {
-    console.log(response);
-    })
+    var formData = new FormData()
+    formData.append('NO','JPTM64000283')
+    $.ajax({
+                url: 'https://eservice.pea.co.th/cos/checkstatus/',
+                method: 'POST',
+                data:formData,
+                async: true,
+                cache: false,
+                processData: false,
+                contentType: false,
+                success: function(response) 
+                {
+                    var obj = JSON.parse(response)
+                    console.log(obj)     
+                }					
+            })
 }
