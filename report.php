274d9f39aa130e59e938b9a1736e06a9eb04ac39
tueseed <tue_seed@hotmail.com>
2021-02-19 14:08:00 +0700

    ,plkvsdg

---
+++ report.php
@@ -0,0 +1,25 @@
+<div class="row mt-3">
+    <div class="col-lg-12 text-center">
+        <span class="text-primary h4">รายงานเหตุการกระแสไฟฟ้าขัดข้อง</span>
+    </div>
+</div>
+<div class="row mt-1">
+    <div class="col-lg-12 text-center">
+        <p class="text-danger" style="font-size:10px;">**ในเขตพื้นที่รับผิดชอบของ PEA อำเภอโพธาราม</p>
+    </div>
+</div>
+<div class="row mt-2">
+    <div class="col-lg-12 text-center">
+        <input type="text" class="form-control" id="peano"placeholder="รหัสเครื่องวัด">
+        <input type="text" class="form-control mt-2" id="place" placeholder="สถานที่ (ซอย ถนน)">
+        <input type="text" class="form-control mt-2" id="observ" placeholder="จุดสังเกตุ">
+        <input type="text" class="form-control mt-2" id="customername" placeholder="ชื่อผู้แจ้ง">
+        <input type="number" class="form-control mt-2" id="tel" placeholder="เบอร์ติดต่อ">
+    </div>
+</div>
+
+<div class="row mt-2">
+    <div class="col-lg-12 text-center">
+        <button class="btn btn-primary btn-block" id="btnSend" onclick="shared()" >ส่งข้อมูล</button>
+    </div>
+</div>
---
+++ scripts/report.js
@@ -0,0 +1,216 @@
+$('#header').hide()
+// main()
+liff.init({liffId: "1655595874-1mYGWaze"})
+async function main()
+{
+    var lat = localStorage.getItem('lat')
+    var long = localStorage.getItem('long')
+    var peano = localStorage.getItem('peano')
+    var place = localStorage.getItem('place')
+    var observ = localStorage.getItem('observ')
+    var customername = localStorage.getItem('customername')
+    var tel = localStorage.getItem('tel')
+    await liff.init({liffId: "1655595874-1mYGWaze"}) // Use own liffId})
+    var flexmes =  await makemessage(lat,long,peano,place,observ,customername,tel)
+    await liff.shareTargetPicker([
+    
+        {
+            type: "flex",
+            altText: "กระแสไฟฟ้าขัดข้อง",
+            contents:flexmes
+        }
+    
+    ])
+    liff.closeWindow()
+
+
+}
+
+function shared()
+{
+    liff.shareTargetPicker([
+    
+        {
+            type: "text",
+            altText: "กระแสไฟฟ้าขัดข้อง",
+            contents:"flexmes"
+        }])
+}
+
+async function makemessage(lat,long,peano,place,observ,customername,tel)
+{
+  var mess = {
+                "type": "bubble",
+                    "hero": {
+                                "type": "image",
+                                "url": "https://cscn.herokuapp.com/pic/poweroutageflex.png",
+                                "size": "full",
+                                "aspectRatio": "20:13",
+                                "aspectMode": "cover",
+                                "action": {
+                                            "type": "uri",
+                                            "uri": "http://linecorp.com/"
+                                            }
+                            },
+                    "body": {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [
+                                                {
+                                                    "type": "text",
+                                                    "text": "แจ้งกระแสไฟฟ้าขัดข้อง",
+                                                    "color":"#703E91",
+                                                    "weight": "bold",
+                                                    "size": "xl"
+                                                },                            
+                                                {
+                                                    "type": "box",
+                                                    "layout": "vertical",
+                                                    "margin": "lg",
+                                                    "spacing": "sm",
+                                                    "contents": [
+                                                                    {
+                                                                        "type": "box",
+                                                                        "layout": "baseline",
+                                                                        "spacing": "sm",
+                                                                        "contents": [
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": "PEA มิเตอร์ :",
+                                                                                        "color": "#06d6a0",
+                                                                                        "weight": "bold",
+                                                                                        "size": "sm",
+                                                                                        "flex": 1
+                                                                                        },
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": peano,
+                                                                                        "wrap": true,
+                                                                                        "color": "#666666",
+                                                                                        "size": "sm",
+                                                                                        "flex": 2
+                                                                                        }
+                                                                                    ]
+                                                                    },            
+                                                                    {
+                                                                        "type": "box",
+                                                                        "layout": "baseline",
+                                                                        "spacing": "sm",
+                                                                        "contents": [
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": "สถานที่ :",
+                                                                                        "color": "#06d6a0",
+                                                                                        "weight": "bold",
+                                                                                        "size": "sm",
+                                                                                        "flex": 1
+                                                                                        },
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": place,
+                                                                                        "wrap": true,
+                                                                                        "color": "#666666",
+                                                                                        "size": "sm",
+                                                                                        "flex": 2
+                                                                                        }
+                                                                                    ]
+                                                                    },
+                                                                    {
+                                                                        "type": "box",
+                                                                        "layout": "baseline",
+                                                                        "spacing": "sm",
+                                                                        "contents": [
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": "จุดสังเกตุ",
+                                                                                        "color": "#06d6a0",
+                                                                                        "weight": "bold",
+                                                                                        "size": "sm",
+                                                                                        "flex": 1
+                                                                                        },
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": observ,
+                                                                                        "wrap": true,
+                                                                                        "color": "#666666",
+                                                                                        "size": "sm",
+                                                                                        "flex": 2
+                                                                                        }
+                                                                                    ]
+                                                                    },
+                                                                    {
+                                                                        "type": "box",
+                                                                        "layout": "baseline",
+                                                                        "spacing": "sm",
+                                                                        "contents": [
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": "ผู้แจ้ง",
+                                                                                        "color": "#06d6a0",
+                                                                                        "weight": "bold",
+                                                                                        "size": "sm",
+                                                                                        "flex": 1
+                                                                                        },
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": customername,
+                                                                                        "wrap": true,
+                                                                                        "color": "#666666",
+                                                                                        "size": "sm",
+                                                                                        "flex": 2
+                                                                                        }
+                                                                                    ]
+                                                                    },
+                                                                    {
+                                                                        "type": "box",
+                                                                        "layout": "baseline",
+                                                                        "spacing": "sm",
+                                                                        "contents": [
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": "โทรศัพท์",
+                                                                                        "color": "#06d6a0",
+                                                                                        "weight": "bold",
+                                                                                        "size": "sm",
+                                                                                        "flex": 1
+                                                                                        },
+                                                                                        {
+                                                                                        "type": "text",
+                                                                                        "text": tel,
+                                                                                        "wrap": true,
+                                                                                        "color": "#666666",
+                                                                                        "size": "sm",
+                                                                                        "flex": 2
+                                                                                        }
+                                                                                    ]
+                                                                    }
+                                                                ]
+                                                    }
+                                                ]
+                                },
+                        "footer": {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "spacing": "sm",
+                                    "contents": [
+                                                    {
+                                                        "type": "button",
+                                                        "style": "link",
+                                                        "height": "sm",
+                                                        "action": {
+                                                                    "type": "uri",
+                                                                    "label": "แผนที่",
+                                                                    "uri": "https://www.google.co.th/maps/search/" + lat + "," + long
+                                                        }
+                                                    },
+                                                        {
+                                                            "type": "spacer",
+                                                            "size": "sm"
+                                                        }
+                                    ],
+                                    "flex": 0
+                        }
+            }
+return mess
+
+}
