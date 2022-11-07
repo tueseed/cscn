ae49d515839d52a74cadc1d0490068dc643cc3e8
Nutthapong <tue_seed@hotmail.com>
2022-11-07 11:50:59 +0700

    dgsfghsfhgs

--- calendar.php
+++ calendar.php
@@ -6,19 +6,13 @@
    * https://developers.google.com/explorer-help/code-samples#javascript
    */
 
-  function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.events.readonly https://www.googleapis.com/auth/calendar.readonly"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
   function loadClient() {
     gapi.client.setApiKey("AIzaSyAp1LKnVj68GEWrH43bRXEGbPf6W9iWyKY");
     return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
               function(err) { console.error("Error loading GAPI client for API", err); });
   }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
+  // Make sure the client is loaded before calling this method.
   function execute() {
     return gapi.client.calendar.events.get({
       "calendarId": "peaptr99@gmail.com",
@@ -30,10 +24,10 @@
               },
               function(err) { console.error("Execute error", err); });
   }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "750613794981-u9m5h7avk3h9mgt7kmae49cfur8lr795.apps.googleusercontent.com"});
-  });
+  gapi.load("client");
 </script>
+
+
 
 
 
@@ -60,5 +54,5 @@
     </div>
 </div>
 <div id="content"></div>
-<button onclick="authenticate().then(loadClient)">authorize and load</button>
+<button onclick="loadClient()">load</button>
 <button onclick="execute()">execute</button>
