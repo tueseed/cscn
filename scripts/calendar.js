2b57884b58c42c1f3fa529ea270838aad8599317
Nutthapong <tue_seed@hotmail.com>
2022-11-08 11:57:30 +0700

    dfsandjvk;nvja

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,29 +1,37 @@
 $(document).ready(function() {
     $('#header').hide()
-    gapi.load("client")
+    
 })
 
-function loadClient() 
-{
-    gapi.client.setApiKey("AIzaSyAp1LKnVj68GEWrH43bRXEGbPf6W9iWyKY")
+// function loadClient() 
+// {
+//     gapi.client.setApiKey("AIzaSyAp1LKnVj68GEWrH43bRXEGbPf6W9iWyKY")
+//     return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
+//         .then(function() { console.log("GAPI client loaded for API"); },
+//               function(err) { console.error("Error loading GAPI client for API", err); })
+//   }
+
+
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyAp1LKnVj68GEWrH43bRXEGbPf6W9iWyKY");
     return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); })
+              function(err) { console.error("Error loading GAPI client for API", err); });
   }
+
+
+
   // Make sure the client is loaded before calling this method.
-function execute() 
-{
-    var eventId =getUrlVars()["eventId"]
-    console.log(eventId)    
+  function execute() {
     return gapi.client.calendar.events.get({
       "calendarId": "peaptr99@gmail.com",
-      "eventId": "0lbg9j64nge1nnj33d8qf2tct"
+      "eventId": "0lbg9j64nge1nnj33d8qf2tct2"
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
-                console.log("Response", response)
+                console.log("Response", response);
               },
-              function(err) { console.error("Execute error", err); })
+              function(err) { console.error("Execute error", err); });
   }
 
   gapi.load("client")
